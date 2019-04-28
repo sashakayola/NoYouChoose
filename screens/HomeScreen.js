90c8b8dbe98afd3f3b2a36bfa4f8b59665f12355
Sasha Kayola <ok515@nyu.edu>
2019-04-27 20:03:41 -0400

    added a scroll view for all the food places in the selected radius

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,323 +1,61 @@
-// import React from 'react';
-// import {
-//   Image,
-//   Platform,
-//   ScrollView,
-//   StyleSheet,
-//   Text,
-//   TouchableOpacity,
-//   View,
-// } from 'react-native';
-// import { WebBrowser } from 'expo';
-
-// import { MonoText } from '../components/StyledText';
-
-// export default class HomeScreen extends React.Component {
-//   static navigationOptions = {
-//     header: null,
-//   };
-
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-//           <View style={styles.getStartedContainer}>
-//             {/* <Text style={styles.getStartedText}>Get started by opening</Text> */}
-
-//             {/* <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-//               <MonoText style={styles.codeHighlightText}>screens/HomeScreen.js</MonoText>
-//             </View> */}
-
-//             <Text style={styles.getStartedText}>
-//              See the map view below!
-//             </Text>
-//           </View>
-
-//           <View style={styles.helpContainer}>
-//             <TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>
-//               <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
-//             </TouchableOpacity>
-//           </View>
-//         </ScrollView>
-
-//         <View style={styles.tabBarInfoContainer}>
-//           <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
-
-//           <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-//             <MonoText style={styles.codeHighlightText}>navigation/MainTabNavigator.js</MonoText>
-//           </View>
-//         </View>
-//       </View>
-//     );
-//   }
-
-//   _maybeRenderDevelopmentModeWarning() {
-//     if (__DEV__) {
-//       const learnMoreButton = (
-//         <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-//           Learn more
-//         </Text>
-//       );
-
-//       return (
-//         <Text style={styles.developmentModeText}>
-//           Development mode is enabled, your app will be slower but you can use useful development
-//           tools. {learnMoreButton}
-//         </Text>
-//       );
-//     } else {
-//       return (
-//         <Text style={styles.developmentModeText}>
-//           You are not in development mode, your app will run at full speed.
-//         </Text>
-//       );
-//     }
-//   }
-
-//   _handleLearnMorePress = () => {
-//     WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
-//   };
-
-//   _handleHelpPress = () => {
-//     WebBrowser.openBrowserAsync(
-//       'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
-//     );
-//   };
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//   },
-//   developmentModeText: {
-//     marginBottom: 20,
-//     color: 'rgba(0,0,0,0.4)',
-//     fontSize: 14,
-//     lineHeight: 19,
-//     textAlign: 'center',
-//   },
-//   contentContainer: {
-//     paddingTop: 30,
-//   },
-//   welcomeContainer: {
-//     alignItems: 'center',
-//     marginTop: 10,
-//     marginBottom: 20,
-//   },
-//   welcomeImage: {
-//     width: 100,
-//     height: 80,
-//     resizeMode: 'contain',
-//     marginTop: 3,
-//     marginLeft: -10,
-//   },
-//   getStartedContainer: {
-//     alignItems: 'center',
-//     marginHorizontal: 50,
-//   },
-//   homeScreenFilename: {
-//     marginVertical: 7,
-//   },
-//   codeHighlightText: {
-//     color: 'rgba(96,100,109, 0.8)',
-//   },
-//   codeHighlightContainer: {
-//     backgroundColor: 'rgba(0,0,0,0.05)',
-//     borderRadius: 3,
-//     paddingHorizontal: 4,
-//   },
-//   getStartedText: {
-//     fontSize: 17,
-//     color: 'rgba(96,100,109, 1)',
-//     lineHeight: 24,
-//     textAlign: 'center',
-//   },
-//   tabBarInfoContainer: {
-//     position: 'absolute',
-//     bottom: 0,
-//     left: 0,
-//     right: 0,
-//     ...Platform.select({
-//       ios: {
-//         shadowColor: 'black',
-//         shadowOffset: { height: -3 },
-//         shadowOpacity: 0.1,
-//         shadowRadius: 3,
-//       },
-//       android: {
-//         elevation: 20,
-//       },
-//     }),
-//     alignItems: 'center',
-//     backgroundColor: '#fbfbfb',
-//     paddingVertical: 20,
-//   },
-//   tabBarInfoText: {
-//     fontSize: 17,
-//     color: 'rgba(96,100,109, 1)',
-//     textAlign: 'center',
-//   },
-//   navigationFilename: {
-//     marginTop: 5,
-//   },
-//   helpContainer: {
-//     marginTop: 15,
-//     alignItems: 'center',
-//   },
-//   helpLink: {
-//     paddingVertical: 15,
-//   },
-//   helpLinkText: {
-//     fontSize: 14,
-//     color: '#2e78b7',
-//   },
-// });
-
 import React, { Component } from 'react';
-import { View, StyleSheet, Text } from 'react-native';
-import { Constants, MapView, Location, Permissions } from 'expo';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-
-const config = {
-  headers: {
-    Authorization: 'Bearer fmAW0diM5L5-44HUuua0b-fpqdCl7nb24nwic9nO54zMM4FE-kb3inLE2ToZ9HENk1LEbXEvghTqIijaQ4Mch8sA6NfRbBBMRr_Skmhs9P_KGjmN9DTOETqNTyDDXHYx',
-  }
-}
-
-let coordinates;
+import { Alert, Button, TextInput, View, StyleSheet } from 'react-native';
 
 export default class App extends Component {
-  constructor(){
-    super()
+  constructor(props) {
+    super(props);
+
     this.state = {
-      mapRegion: { latitude: 37.78825, longitude: -122.4324, latitudeDelta: 0.0922, longitudeDelta: 0.0421 },
-      locationResult: null,
-      location: {coords: { latitude: 40.6980, longitude: -73.9963}},
-      marker: '',
-      isLoading: true,
-      food: null
-    }
-    this.getFoodData = this.getFoodData.bind(this)
+      username: '',
+      password: '',
+    };
   }
 
-  componentDidMount() {
-    this._getLocationAsync();
-  }
-
-  getFoodData() {
-    return fetch(`https://api.yelp.com/v3/businesses/search?term=food&latitude=${this.state.marker.latitude}&longitude=${this.state.marker.longitude}&radius=800&limit=20`, config)
-    .then(response => response.json())
-    .then(responseJson => {
-      this.setState({
-        isLoading: false,
-        food: responseJson.businesses
-      })
-    })
-  }
-
-  _getLocationAsync = async () => {
-    let { status } = await Permissions.askAsync(Permissions.LOCATION);
-    if (status !== 'granted') {
-      this.setState({
-        locationResult: 'Permission to access location was denied',
-        location,
-      });
-    }
-
-    let location = await Location.getCurrentPositionAsync({});
-    this.setState({ locationResult: JSON.stringify(location), location });
-  };
-
-  async addMarker(coordinates) {
-    // console.warn(coordinates);
-    await this.setState({
-      marker: coordinates
-    })
-    this.getFoodData()
+  onLogin() {
+    const { username, password } = this.state;
+    Alert.alert('Credentials', `${username} + ${password}`);
   }
 
   render() {
     return (
-      <View style={{flex: 1}}>
-    {/* //     {this.state.isLoading && console.warn(this.state.food)} */}
-        { <MapView
-        ref={ref => { this.map = ref; }}
-        style={styles.map}
-        region={{ latitude: this.state.location.coords.latitude, longitude: this.state.location.coords.longitude, latitudeDelta: 0.0922, longitudeDelta: 0.0421 }}
-        showsUserLocation={true}
-        onPress={(e) => {
-          this.addMarker(e.nativeEvent.coordinate)
-          // await this.getFoodData()
-        }}
-        >
+      <View style={styles.container}>
+        <TextInput
+          value={this.state.username}
+          onChangeText={(username) => this.setState({ username })}
+          placeholder={'Username'}
+          style={styles.input}
+        />
+        <TextInput
+          value={this.state.password}
+          onChangeText={(password) => this.setState({ password })}
+          placeholder={'Password'}
+          secureTextEntry={true}
+          style={styles.input}
+        />
 
-{this.state.marker !== '' &&
-        <MapView.Marker
-            coordinate={{latitude: this.state.marker.latitude,
-            longitude: this.state.marker.longitude}}
-            title={"Let's get food around here!"}
-            // description={"description"}
-            // pinColor={'aqua'}
-            // pinColor={'plum'}
-            // pinColor={'orange'}
-            // pinColor={'tomato'}
-            // pinColor={'green'}
-            // pinColor={'indigo'}
-            // color={'#76BBB7'}
-         >
-         <Icon name="silverware-fork-knife" size={40} color={"indigo"} />
-        </MapView.Marker>
-        }
-
-          {this.state.marker !== '' &&
-            <MapView.Circle
-                    key = { (this.state.marker.latitude + this.state.marker.longitude).toString() }
-                    center = { this.state.marker }
-                    radius = { 1000 }
-                    strokeWidth = { 1 }
-                    strokeColor = { '#1a66ff' }
-                    fillColor = { 'rgba(230,238,255,0.5)' }
-            />
-          }
-
-      {!this.state.isLoading &&
-       this.state.food.map(val => {
-          return (
-            <View>
-            <MapView.Marker
-                  coordinate={{latitude: val.coordinates.latitude, longitude: val.coordinates.longitude}}
-                  pinColor={'aqua'}
-              >
-              </MapView.Marker>
-
-            </View>
-          )
-        })
-      }
-      </MapView>}
+        <Button
+          title={'Login'}
+          style={styles.input}
+          onPress={this.onLogin.bind(this)}
+        />
       </View>
-    )
+    );
   }
 }
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    // paddingTop: Constants.statusBarHeight,
     backgroundColor: '#ecf0f1',
   },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    color: '#34495e',
+  input: {
+    width: 200,
+    height: 44,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'black',
+    marginBottom: 10,
   },
-  map: {
-    flex: 1
-  }
 });
--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,94 +1,207 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Text, Button, TouchableOpacity, Alert, Dimensions, Animated, Image } from 'react-native';
 import { Constants, MapView, Location, Permissions } from 'expo';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
+const config = {
+  headers: {
+    Authorization: 'Bearer fmAW0diM5L5-44HUuua0b-fpqdCl7nb24nwic9nO54zMM4FE-kb3inLE2ToZ9HENk1LEbXEvghTqIijaQ4Mch8sA6NfRbBBMRr_Skmhs9P_KGjmN9DTOETqNTyDDXHYx',
+  }
+}
+
+const Images = [
+  { uri: "https://i.imgur.com/sNam9iJ.jpg" },
+  { uri: "https://i.imgur.com/N7rlQYt.jpg" },
+  { uri: "https://i.imgur.com/UDrH0wm.jpg" },
+  { uri: "https://i.imgur.com/Ka8kNST.jpg" }
+]
+
+const { width, height } = Dimensions.get("window");
+
+const CARD_HEIGHT = height / 4.7;
+const CARD_WIDTH = CARD_HEIGHT + 35;
 
 export default class App extends Component {
-  state = {
-    mapRegion: { latitude: 37.78825, longitude: -122.4324, latitudeDelta: 0.0922, longitudeDelta: 0.0421 },
-    locationResult: null,
-    location: {coords: { latitude: 40.6980, longitude: -73.9963}},
-    marker: ''
-  };
-
+  constructor(){
+    super()
+    this.state = {
+      mapRegion: { latitude: 37.78825, longitude: -122.4324, latitudeDelta: 0.0922, longitudeDelta: 0.0421 },
+      locationResult: null,
+      location: {coords: { latitude: 40.6980, longitude: -73.9963}},
+      marker: '',
+      isLoading: true,
+      food: null,
+      canAdjustFood: true
+    }
+    this.getFoodData = this.getFoodData.bind(this)
+    this.confirmRadius = this.confirmRadius.bind(this)
+  }
 
   componentDidMount() {
     this._getLocationAsync();
+    this.index = 0;
+    this.animation = new Animated.Value(0);
   }
 
-  addMarker(coordinates) {
-    // console.warn(coordinates);
+  getFoodData() {
+    if (this.state.canAdjustFood) {
+      return fetch(`https://api.yelp.com/v3/businesses/search?term=food&latitude=${this.state.marker.latitude}&longitude=${this.state.marker.longitude}&radius=800&limit=5`, config)
+      .then(response => response.json())
+      .then(responseJson => {
+        this.setState({
+          isLoading: false,
+          food: responseJson.businesses
+        })
+      })
+    }
+  }
+
+  confirmRadius() {
+    Alert.alert('Confirmed! Now wait for your friend to choose a place!')
     this.setState({
-      marker: coordinates
+      canAdjustFood: false
     })
   }
 
   _getLocationAsync = async () => {
-   let { status } = await Permissions.askAsync(Permissions.LOCATION);
-   if (status !== 'granted') {
-     this.setState({
-       locationResult: 'Permission to access location was denied',
-       location,
-     });
-   }
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({
+        locationResult: 'Permission to access location was denied',
+        location,
+      });
+    }
 
-   let location = await Location.getCurrentPositionAsync({});
-   this.setState({ locationResult: JSON.stringify(location), location });
- };
+    let location = await Location.getCurrentPositionAsync({});
+    this.setState({ locationResult: JSON.stringify(location), location });
+  };
 
-// fitPadding() {
-//   this.map.fitToCoordinates([{latitude: 40.6980, longitude: -73.9963}, this.state.marker], {
-//     edgePadding: { top: 50, right: 50, bottom: 120, left: 50 },
-//     animated: true
-//   });
-// }
+  async addMarker(coordinates) {
+    await this.setState({
+      marker: coordinates
+    })
+    this.getFoodData()
+  }
 
-render() {
-  return (
-    <View style={{flex: 1}}>
-        <MapView
+  render() {
+    return (
+      <View style={{flex: 1}}>
+      {!this.state.isLoading && this.state.canAdjustFood &&
+        <TouchableOpacity
+          style={styles.confirmButton}
+          onPress={() => {
+            if (this.state.canAdjustFood) {
+              this.confirmRadius()
+            }
+          }}
+          underlayColor='pink'>
+          <Text style={styles.buttonText}>Click to confirm food radius!</Text>
+        </TouchableOpacity>
+      }
+
+        { <MapView
         ref={ref => { this.map = ref; }}
         style={styles.map}
         region={{ latitude: this.state.location.coords.latitude, longitude: this.state.location.coords.longitude, latitudeDelta: 0.0922, longitudeDelta: 0.0421 }}
         showsUserLocation={true}
         onPress={(e) => {
-          this.addMarker(e.nativeEvent.coordinate)
+          if (this.state.canAdjustFood) {
+            this.addMarker(e.nativeEvent.coordinate)
+          }
         }}
         >
 
-        {this.state.marker !== '' &&
+      {this.state.marker !== '' &&
         <MapView.Marker
             coordinate={{latitude: this.state.marker.latitude,
             longitude: this.state.marker.longitude}}
-            title={"Let's get food around here!"}
-            // description={"description"}
-            // pinColor={'aqua'}
-            // pinColor={'plum'}
-            // pinColor={'orange'}
-            // pinColor={'tomato'}
-            // pinColor={'green'}
-            // pinColor={'indigo'}
-            // color={'#76BBB7'}
          >
-         <Icon name="silverware-fork-knife" size={40} color={"indigo"} />
+         <Icon name="silverware-fork-knife" size={40} color={"#c71585"} />
         </MapView.Marker>
         }
-
 
           {this.state.marker !== '' &&
             <MapView.Circle
                     key = { (this.state.marker.latitude + this.state.marker.longitude).toString() }
                     center = { this.state.marker }
-                    radius = { 450 }
+                    radius = { 1000 }
                     strokeWidth = { 1 }
                     strokeColor = { '#1a66ff' }
                     fillColor = { 'rgba(230,238,255,0.5)' }
             />
           }
-        </MapView>
+
+      {!this.state.isLoading &&
+       this.state.food.map(val => {
+        const nameOfMarker = `${val.name} (${val.rating}/5)`;
+        const addressOfMarker = `${val.location.address1}, ${val.location.city}`;
+          return (
+            <View key={val.name}>
+            <MapView.Marker
+                  coordinate={{latitude: val.coordinates.latitude, longitude: val.coordinates.longitude}}
+                  title={nameOfMarker}
+                  description={addressOfMarker}
+                  pinColor={'#00ced1'}
+              >
+            </MapView.Marker>
+            </View>
+          )
+        })
+      }
+
+          </MapView>}
+
+        {!this.state.isLoading &&
+          <Animated.ScrollView
+          horizontal
+          scrollEventThrottle={1}
+          showsHorizontalScrollIndicator={false}
+          snapToInterval={CARD_WIDTH}
+          onScroll={Animated.event(
+            [
+              {
+                nativeEvent: {
+                  contentOffset: {
+                    x: this.animation,
+                  },
+                },
+              },
+            ],
+            { useNativeDriver: true }
+          )}
+          style={styles.scrollView}
+          contentContainerStyle={styles.endPadding}
+        >
+          {this.state.food.map((marker, index) => (
+            <View style={styles.card} key={index}>
+              <View style={styles.textContent}>
+              <TouchableOpacity
+                style={{
+                  zIndex: 1,
+                  position: 'absolute',
+                  left: 0,
+                  top: 0,
+                  width: CARD_WIDTH,
+                  height: CARD_HEIGHT,
+                  backgroundColor: 'transparent',
+                }}
+                onPress={() => alert('hi')}
+              />
+
+
+                <Text style={styles.cardtitle}>{marker.name}</Text>
+                <Text style={styles.cardDescription}>{marker.location.address1},</Text>
+                <Text numberOfLines={1} style={styles.cardDescription}>{marker.location.city}</Text>
+                <Text numberOfLines={1} style={styles.cardDescription}>{marker.rating} / 5</Text>
+                <Text numberOfLines={1} style={styles.cardDescription}>{marker.price}</Text>
+              </View>
+            </View>
+          ))}
+          </Animated.ScrollView>
+        }
+
       </View>
-    );
+    )
   }
 }
 
@@ -97,11 +210,11 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
+    // paddingTop: Constants.statusBarHeight,
     backgroundColor: '#ecf0f1',
   },
   paragraph: {
-    margin: 24,
+    // margin: 24,
     fontSize: 18,
     fontWeight: 'bold',
     textAlign: 'center',
@@ -109,5 +222,89 @@ const styles = StyleSheet.create({
   },
   map: {
     flex: 1
-  }
+  },
+  confirmButton:{
+    marginRight:10,
+    marginLeft:10,
+    // marginTop:10,
+    paddingTop:10,
+    paddingBottom:10,
+    backgroundColor:'#20b2aa80',
+    borderRadius:12,
+    borderWidth: 2,
+    borderColor: '#fff'
+  },
+  buttonText:{
+      color:'#fff',
+      textAlign:'center',
+      fontSize: 20,
+      fontWeight: "bold"
+      // paddingLeft : 10,
+      // paddingRight : 10
+  },
+  scrollView: {
+    position: "absolute",
+    bottom: 5,
+    left: 0,
+    right: 0,
+    paddingVertical: 5,
+  },
+  endPadding: {
+    // paddingRight: width - CARD_WIDTH,
+  },
+  card: {
+    padding: 5,
+    elevation: 2,
+    backgroundColor: '#db709385',
+    marginHorizontal: 10,
+    borderRadius: 12,
+    borderWidth: 2,
+    borderColor: '#fff',
+    shadowColor: "#000",
+    shadowRadius: 5,
+    shadowOpacity: 0.3,
+    shadowOffset: { x: 2, y: -2 },
+    height: CARD_HEIGHT,
+    width: CARD_WIDTH,
+    overflow: "hidden",
+  },
+  cardImage: {
+    flex: 3,
+    width: "100%",
+    height: "100%",
+    alignSelf: "center",
+  },
+  textContent: {
+    flex: 1,
+    flexWrap: 'wrap'
+  },
+  cardtitle: {
+    fontSize: 14,
+    marginTop: 5,
+    fontWeight: "bold",
+    color: "white"
+  },
+  cardDescription: {
+    fontSize: 14,
+    color: "white",
+  },
+  markerWrap: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  marker: {
+    width: 8,
+    height: 8,
+    borderRadius: 4,
+    backgroundColor: "rgba(130,4,150, 0.9)",
+  },
+  ring: {
+    width: 24,
+    height: 24,
+    borderRadius: 12,
+    backgroundColor: "rgba(130,4,150, 0.3)",
+    position: "absolute",
+    borderWidth: 1,
+    borderColor: "rgba(130,4,150, 0.5)",
+  },
 });
